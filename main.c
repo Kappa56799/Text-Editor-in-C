e40c44088a59f5897952e396146d3bf2438f26f2
Kacper Palka <114831362+Kappa56799@users.noreply.github.com>
2023-05-01 16:29:03 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,3 @@
-/*** includes ***/
-
-#define _DEFAULT_SOURCE
-#define _BSD_SOURCE
-#define _GNU_SOURCE
-
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -17,14 +11,17 @@
 #include <time.h>
 #include <unistd.h>
 
-/*** defines ***/
-
+//Defines
 #define KAPPA_VERSION "0.0.1"
-#define KILO_TAB_STOP 8
-#define KILO_QUIT_TIMES 3
+#define TAB_STOP 8 
+#define QUIT_TIMES 3 //how many times you need to press the quit button to quit
+#define ABUF_INIT {NULL, 0}
+#define CTRL_KEY(k) ((k) & 0x1f) //
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
 
-#define CTRL_KEY(k) ((k) & 0x1f)
-
+//Enums to allow other keys to work
 enum editorKey {
   BACKSPACE = 127,
   ARROW_LEFT = 1000,
@@ -38,8 +35,7 @@ enum editorKey {
   PAGE_DOWN
 };
 
-/*** data ***/
-
+//Data used throughout the program
 typedef struct erow {
   int size;
   int rsize;
@@ -65,44 +61,69 @@ struct editorConfig {
 
 struct editorConfig E;
 
-/*** prototypes ***/
+/*** append buffer ***/
+struct abuf {
+  char *b;
+  int len;
+};
 
+
+//Function protypes 
 void SetStatusMessage(const char *fmt, ...);
 void ClearScreen();
 char *Prompt(char *prompt);
 
-
-
-/*** terminal ***/
-
+//Terminal Settings
+//Closes the program and returns an error
 void Close(const char *s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
 
+  //prints a decriptive error message and exits the program
   perror(s);
   exit(1);
 }
 
+//Re enables all the terminal flags we turned off after program ends
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     Close("tcsetattr");
 }
 
+//We need to remove most of the flags to allow this program to work as a text editor in the terminal
 void enableRawMode() {
+
   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) Close("tcgetattr");
   atexit(disableRawMode);
 
+  /*
+  We set these flags to allow us to prevent stuff like ctrl + c exiting the program, echoing which is useful normally, turning off
+  canonical mode (ICANON) to allow us to read byte by  byte instead of line by line, ISIG allows us to read the bytes from ctrl-c (3) nd ctrl-z (26),
+  IXON allows us to read bytes from the input of Ctrl-S (19) and ctrl-Q (17),
+  IEXTEN prevents waiting from another input when ctrl-v is pressed this allows us to read ctrl-V as 22 bytes and Ctrl-o as 15 bytes
+  ICRNL Turns off the feature of translating carraige returns and allows us to read CTRL + M as 13 bytes instead of 10
+  We turn off OPOST flag
+  BRKINT is turnedd off to stop ctrl+ c working
+  INPCK enables parity checking (not needed these days)
+  ISTRIP chages every 8th bit to a 0
+  CS8 sets the character size to 8 bits per byte
+  */
   struct termios raw = E.orig_termios;
+
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+
+  //Timeout for read() so it returns if no input is given
+  raw.c_cc[VMIN] = 0; //min num of bytes too be read
+  raw.c_cc[VTIME] = 1; //100ms timeout
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) Close("tcsetattr");
 }
 
+
+//reads a character presssed from STDIN  by the user
 int ReadKey() {
   int nread;
   char c;
@@ -130,7 +151,8 @@ int ReadKey() {
             case '8': return END_KEY;
           }
         }
-      } else {
+      } 
+      else {
         switch (seq[1]) {
           case 'A': return ARROW_UP;
           case 'B': return ARROW_DOWN;
@@ -140,7 +162,8 @@ int ReadKey() {
           case 'F': return END_KEY;
         }
       }
-    } else if (seq[0] == 'O') {
+    } 
+    else if (seq[0] == 'O') {
       switch (seq[1]) {
         case 'H': return HOME_KEY;
         case 'F': return END_KEY;
@@ -153,6 +176,7 @@ int ReadKey() {
   }
 }
 
+//Keeps Track of where the cursor  while the program is running
 int getCursorPosition(int *rows, int *cols) {
   char buf[32];
   unsigned int i = 0;
@@ -164,6 +188,7 @@ int getCursorPosition(int *rows, int *cols) {
     if (buf[i] == 'R') break;
     i++;
   }
+
   buf[i] = '\0';
 
   if (buf[0] != '\x1b' || buf[1] != '[') return -1;
@@ -172,13 +197,15 @@ int getCursorPosition(int *rows, int *cols) {
   return 0;
 }
 
+//We get the size of the terminal window and display according to the display
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
-  } else {
+  } 
+  else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
     return 0;
@@ -186,36 +213,41 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** row operations ***/
-
 int RowCXToRX(erow *row, int cx) {
   int rx = 0;
   int j;
+
   for (j = 0; j < cx; j++) {
     if (row->chars[j] == '\t')
-      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+      rx += (TAB_STOP - 1) - (rx % TAB_STOP);
     rx++;
   }
+
   return rx;
 }
 
 void UpdateRow(erow *row) {
   int tabs = 0;
   int j;
+
   for (j = 0; j < row->size; j++)
     if (row->chars[j] == '\t') tabs++;
 
   free(row->render);
-  row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
+  row->render = malloc(row->size + tabs*(TAB_STOP - 1) + 1);
 
   int idx = 0;
   for (j = 0; j < row->size; j++) {
     if (row->chars[j] == '\t') {
       row->render[idx++] = ' ';
-      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
-    } else {
+      while (idx % TAB_STOP != 0) row->render[idx++] = ' ';
+    } 
+    
+    else {
       row->render[idx++] = row->chars[j];
     }
   }
+
   row->render[idx] = '\0';
   row->rsize = idx;
 }
@@ -351,12 +383,14 @@ void OpenFile(char *filename) {
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
+
   while ((linelen = getline(&line, &linecap, fp)) != -1) {
-    while (linelen > 0 && (line[linelen - 1] == '\n' ||
-                           line[linelen - 1] == '\r'))
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
       linelen--;
+    }
     InsertRow(E.numrows, line, linelen);
   }
+
   free(line);
   fclose(fp);
   E.dirty = 0;
@@ -391,14 +425,6 @@ void SaveFile() {
   SetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
-/*** append buffer ***/
-
-struct abuf {
-  char *b;
-  int len;
-};
-
-#define ABUF_INIT {NULL, 0}
 
 void abAppend(struct abuf *ab, const char *s, int len) {
   char *new = realloc(ab->b, ab->len + len);
@@ -417,6 +443,7 @@ void abFree(struct abuf *ab) {
 
 void Scroll() {
   E.rx = 0;
+
   if (E.cy < E.numrows) {
     E.rx = RowCXToRX(&E.row[E.cy], E.cx);
   }
@@ -424,12 +451,15 @@ void Scroll() {
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
+
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+
   if (E.rx < E.coloff) {
     E.coloff = E.rx;
   }
+
   if (E.rx >= E.coloff + E.screencols) {
     E.coloff = E.rx - E.screencols + 1;
   }
@@ -437,28 +467,37 @@ void Scroll() {
 
 void DrawRows(struct abuf *ab) {
   int y;
+
   for (y = 0; y < E.screenrows; y++) {
     int filerow = y + E.rowoff;
+
     if (filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
           "KAPPA editor -- version %s", KAPPA_VERSION);
+
         if (welcomelen > E.screencols) welcomelen = E.screencols;
         int padding = (E.screencols - welcomelen) / 2;
         if (padding) {
           abAppend(ab, "~", 1);
           padding--;
         }
+
         while (padding--) abAppend(ab, " ", 1);
         abAppend(ab, welcome, welcomelen);
-      } else {
+      } 
+
+      else {
         abAppend(ab, "~", 1);
       }
-    } else {
+    } 
+    else {
       int len = E.row[filerow].rsize - E.coloff;
+
       if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
+
       abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
@@ -470,18 +509,23 @@ void DrawRows(struct abuf *ab) {
 void DrawStatusBar(struct abuf *ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
+
   int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
     E.filename ? E.filename : "[No Name]", E.numrows,
     E.dirty ? "(modified)" : "");
+
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
     E.cy + 1, E.numrows);
+
   if (len > E.screencols) len = E.screencols;
   abAppend(ab, status, len);
+
   while (len < E.screencols) {
     if (E.screencols - len == rlen) {
       abAppend(ab, rstatus, rlen);
       break;
-    } else {
+    } 
+    else {
       abAppend(ab, " ", 1);
       len++;
     }
@@ -493,8 +537,10 @@ void DrawStatusBar(struct abuf *ab) {
 void DrawMessagebar(struct abuf *ab) {
   abAppend(ab, "\x1b[K", 3);
   int msglen = strlen(E.statusmsg);
+
   if (msglen > E.screencols) msglen = E.screencols;
   if (msglen && time(NULL) - E.statusmsg_time < 5)
+
     abAppend(ab, E.statusmsg, msglen);
 }
 
@@ -511,8 +557,7 @@ void ClearScreen() {
   DrawMessagebar(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-                                            (E.rx - E.coloff) + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -523,43 +568,57 @@ void ClearScreen() {
 
 void SetStatusMessage(const char *fmt, ...) {
   va_list ap;
+
   va_start(ap, fmt);
   vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
   va_end(ap);
+
   E.statusmsg_time = time(NULL);
 }
 
 /*** input ***/
 char *Prompt(char *prompt) {
   size_t bufsize = 128;
+
   char *buf = malloc(bufsize);
   size_t buflen = 0;
   buf[0] = '\0';
+
   while (1) {
     SetStatusMessage(prompt, buf);
     ClearScreen();
     int c = ReadKey();
+
     if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
       if (buflen != 0) buf[--buflen] = '\0';
-    } else if (c == '\x1b') {
+    } 
+
+    else if (c == '\x1b') {
       SetStatusMessage("");
       free(buf);
       return NULL;
-    } else if (c == '\r') {
+    } 
+
+    else if (c == '\r') {
       if (buflen != 0) {
         SetStatusMessage("");
         return buf;
       }
-    } else if (!iscntrl(c) && c < 128) {
+    } 
+
+    else if (!iscntrl(c) && c < 128) {
       if (buflen == bufsize - 1) {
         bufsize *= 2;
         buf = realloc(buf, bufsize);
       }
+
       buf[buflen++] = c;
       buf[buflen] = '\0';
     }
   }
 }
+
+//Allows us to move the cursor using arrow keys in the program
 void MoveCursor(int key) {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
@@ -599,8 +658,9 @@ void MoveCursor(int key) {
   }
 }
 
+//Processes the character/key the user inputted and displays the relevant action
 void ProcessKey() {
-  static int quit_times = KILO_QUIT_TIMES;
+  static int quit_times = QUIT_TIMES;
 
   int c = ReadKey();
 
@@ -673,11 +733,10 @@ void ProcessKey() {
       break;
   }
 
-  quit_times = KILO_QUIT_TIMES;
+  quit_times = QUIT_TIMES;
 }
 
-/*** init ***/
-
+//Init of the program
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
@@ -695,6 +754,7 @@ void initEditor() {
   E.screenrows -= 2;
 }
 
+//Main of the program
 int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
